250651b9ffd5aae245ae6a800d978fd469ccfb9b
manjusha <manjushakakuturi@gmail.com>
2026-06-03 10:43:08 +0530

    updates server.ts file added dynamic port

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,22 @@
-import { Server } from "node:http";
 import app from "./app";
 import pool from "./config/db";
 import dotenv from "dotenv";
 dotenv.config();
-const port = 5000;
-const startServer = async()=>{
-    try{
-    await pool.getConnection();
-    console.log("database connected");
 
-app.listen(port,()=>{
-    console.log(`Server running on port ${port}`);
-});
-}
-catch(error){
+const port = process.env.PORT || 5000; // ✅ use Railway's PORT
+
+const startServer = async () => {
+  try {
+    await pool.getConnection();
+    console.log("Database connected");
+
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  } catch (error) {
     console.log(error);
-}};
+    process.exit(1);
+  }
+};
+
 startServer();
