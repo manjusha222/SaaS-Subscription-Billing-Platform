6740a95374c284a3e2f19dadc5ab948f932c8fbe
manjusha <manjushakakuturi@gmail.com>
2026-06-03 15:39:11 +0530

    updated db.ts with console statments

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,5 +1,8 @@
 import mysql from "mysql2/promise";
-
+console.log("MYSQLHOST =", process.env.MYSQLHOST);
+console.log("MYSQLPORT =", process.env.MYSQLPORT);
+console.log("MYSQLUSER =", process.env.MYSQLUSER);
+console.log("MYSQLDATABASE =", process.env.MYSQLDATABASE);
 const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
   port: Number(process.env.MYSQLPORT),
