84bb985df6e63eddf11148fddf704cfb43d26a31
manjusha <manjushakakuturi@gmail.com>
2026-06-03 17:14:44 +0530

    updated db.ts with DATABASE variable console stmnt

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -2,13 +2,13 @@ import mysql from "mysql2/promise";
 console.log("MYSQLHOST =", process.env.MYSQLHOST);
 console.log("MYSQLPORT =", process.env.MYSQLPORT);
 console.log("MYSQLUSER =", process.env.MYSQLUSER);
-console.log("MYSQLDATABASE =", process.env.MYSQLDATABASE);
+console.log("MYSQLDATABASE =", process.env.MYSQL_DATABASE);
 const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
   port: Number(process.env.MYSQLPORT),
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  database: process.env.MYSQL_DATABASE,
 });
 
 export default pool;
