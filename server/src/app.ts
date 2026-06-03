468ff4185ed6ac9283ec9f4f6cf6189ad56ea2ca
manjusha <manjushakakuturi@gmail.com>
2026-06-03 10:29:40 +0530

    updated app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,23 +1,30 @@
 import Express = require("express");
 import authRouter from "./routes/authRouter";
 import planRoutes from "./routes/planRoutes";
-import subscriptionsRoutes from "./routes/subscriptionRoutes"
-import userRoutes from "./routes/userRoutes"
+import subscriptionsRoutes from "./routes/subscriptionRoutes";
+import userRoutes from "./routes/userRoutes";
 import cors from "cors";
+
 const app = Express();
+
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://your-app.vercel.app"
+    "https://your-app.vercel.app" 
   ],
   credentials: true
 }));
+
 app.use(Express.json());
-app.use("/api/auth",authRouter);
-app.use("/api/plans",planRoutes);
-app.use("/api/subscriptions",subscriptionsRoutes);
-app.use("/api/users",userRoutes)
-// app.get("/",(req,res)=>{
-//     res.send("Backend running");
-// });
+
+
+app.get("/", (req, res) => {
+  res.json({ message: "Server is running" });
+});
+
+app.use("/api/auth", authRouter);
+app.use("/api/plans", planRoutes);
+app.use("/api/subscriptions", subscriptionsRoutes);
+app.use("/api/users", userRoutes);
+
 export default app;
